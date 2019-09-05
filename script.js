18565d589c7dc5397d757256d9954a09452d2aea
Mélu <zelumine@live.fr>
2019-09-05 15:15:10 +0200

    Trying to fix the mobile navbar

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // navbar toggle start
-(function($) { // Begin jQuery
-    $(function() { // DOM ready
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      // Hamburger to X toggle
-      document.querySelector('#nav-toggle').addEventListener('click', function() {
-        this.classList.toggle('active');
-      });
-    }); // end DOM ready
-  })(jQuery); // end jQuery
+
+/** code by webdevtrick ( https://webdevtrick.com ) **/
+(function($) { 
+  $(function() { 
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); 
+})(jQuery);
   // navbar toggle end
